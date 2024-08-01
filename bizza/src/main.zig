1bf70eb8846f4e10022e69841789d85de650dc4d
z12345 <zzzooozzz836@gmail.com>
2024-08-01 12:22:08 +0545

    ZIGGGGGGGGGGGGGGGGGGGG

--- bizza/src/main.zig
+++ bizza/src/main.zig
@@ -1,5 +1,29 @@
 const std = @import("std");
 pub fn main() !void {
     const stdout = std.io.getStdOut().writer();
-    try stdout.print("hi", .{});
+
+    const arr = [_]i32{ 1, 2, 3, 4, 5 };
+    const len = arr.len;
+    try stdout.print("hi \n", .{});
+    try stdout.print("lol {}\n", .{len});
+    var i: i32 = 2;
+    var sum: i32 = 0;
+    while (i < 1000000500) : (i *= 2) {
+        try stdout.print("{} \n", .{i});
+        sum += i;
+    }
+    try stdout.print("sum is {}", .{sum});
+}
+
+const expect = std.testing.expect;
+
+test "if statement" {
+    const b = false;
+    var x: u16 = 0;
+    if (b) {
+        x += 1;
+    } else {
+        x += 2;
+    }
+    try expect(x == 2);
 }
