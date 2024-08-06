1fdfacf298b338b66280d265d4c9894e969ad3f3
zzzooozzz836@gmail.com <zzzooozzz836@gmail.com>
2024-08-06 20:40:29 +0545

    pointers const is bad

--- pointers/src/main.zig
+++ pointers/src/main.zig
@@ -5,6 +5,9 @@ pub fn main() !void {
     var x: i32 = 3;
     double(&x);
     try stdout.print("x is {}", .{x});
+    //const _y: u8 = 1;
+    //var z = &y; // makes z const u8  making it const pointer
+    //z.* += 1;
 }
 pub fn double(num: *i32) void {
     num.* *= 2;
