407bb6f2f2bd8a64dd60173ff8480990859d08de
zzzooozzz836@gmail.com <zzzooozzz836@gmail.com>
2024-08-15 21:55:44 +0545

    ahhhh

--- enums/src/main.zig
+++ enums/src/main.zig
@@ -1,9 +1,6 @@
 const std = @import("std");
 
 pub fn main() !void {
-    
-    const directions = enum {east, south, west, north};
-
-
-
+    const stdout = std.io.getStdOut().writer();
+    const directions = enum { east, south, west, north };
 }
